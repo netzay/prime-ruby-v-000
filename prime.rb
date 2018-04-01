7a870f2a6bb555b5d275111a9f7a2e39a9236f9e
Ally Netzel <netzelally@gmail.com>
2018-04-01 04:09:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,10 @@
 def prime?(n)
   return false if n < 2 
   return true if n == 3 || n == 2 
-  if (2...n-1).any? do |i| 
-    n % i == 0
-      return false
-    end
+    if (2...n-1).any? do |i| 
+      n % i == 0
+        return false
+      end
     return true
   end
 end
