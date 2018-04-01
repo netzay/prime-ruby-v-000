8fc75f0500f99b11ce7fd5088dc65905107b1621
Ally Netzel <netzelally@gmail.com>
2018-04-01 03:46:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,7 @@
 def prime?(num)
- if num % 1 == 0 
-   return true
- end
- if num % 1 != 0 
-   return false
- end
+  (2..num-1).each do |n|
+    if num <= 1 || num % n == 0 
+      return false
+    end
+  return true
 end
-
-#Have the program find the remainder when dividing the input (say n) by 2, 3, 4, ..., n‾√ (or the following integer if n‾√ is not an integer.) If this value ever leaves a remainder of zero then your number is composite and you can stop checking divisors. If the remainder is non-zero for all of these values then your number is prime.
