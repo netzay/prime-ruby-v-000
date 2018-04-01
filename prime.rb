91d8133fac34684856cc37c9e8043b95bf43e26f
Ally Netzel <netzelally@gmail.com>
2018-04-01 04:01:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
+=begin
 def prime?(num)
   array = []
   array == (-1..num).to_a 
@@ -7,4 +8,15 @@ def prime?(num)
       end
     end
   return true
+end
+=end
+
+def prime?(n)
+  return false if n < 2 
+  return true if n == 3 || n == 2 
+    if (2...n-1).any?{|i| n % i == 0}
+      false
+    else
+      true
+    end
 end
