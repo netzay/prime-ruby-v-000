73885cfce362d39143ecacd01c108d0d6f63a99f
Ally Netzel <netzelally@gmail.com>
2018-04-01 03:42:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,3 +6,5 @@ def prime?(num)
    return false
  end
 end
+
+#Have the program find the remainder when dividing the input (say n) by 2, 3, 4, ..., n‾√ (or the following integer if n‾√ is not an integer.) If this value ever leaves a remainder of zero then your number is composite and you can stop checking divisors. If the remainder is non-zero for all of these values then your number is prime.
