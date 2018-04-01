7b26e2731db70d1ed6b8c2c1fedf4903d4c447d5
Ally Netzel <netzelally@gmail.com>
2018-04-01 04:03:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,3 @@
-=begin
-def prime?(num)
-  array = []
-  array == (-1..num).to_a 
-    array.each do |n|
-      if n <= 0 || num % n == 0 
-        return false
-      end
-    end
-  return true
-end
-=end
 
 def prime?(n)
   return false if n < 2 
